7748f7fa7a89da0959df4de1aa996ead3b61f8cd
Vitali.Palcheuski <05vp94@mail.ru>
2023-04-25 15:57:19 +0300

    promise full

--- promis.js
+++ promis.js
@@ -19,4 +19,30 @@ promis2.then((user) => {
 
 
 
-conts otherPromis = Promise.all([promis1,promis2])
+const otherPromis = Promise.all([promis1,promis2])
+
+function getNumber (){
+    // const promise = Promise.resolve(Math.random())
+
+    const promise = new Promise((resolve, reject) => {
+        resolve(Math.random() )
+    })
+
+    return promise
+}
+
+getNumber().then(n => console.log(n))
+
+
+function  wait(ms) {
+    return new Promise( (res) => {
+       setTimeout( () => {res()}, ms)
+    })
+}
+
+async  function run() {
+    await  wait(1000)
+    console.log(1)
+    await  wait(1000)
+    console.log(2)
+}
